32bfc583d742bc87664590e7af34ccc7a9af52de
phani-srikar <phanisrikar93ume@gmail.com>
2024-04-10 07:14:36 -0700

    test: use beta SQL layer for cdk deployments

--- packages/amplify-graphql-model-transformer/src/resolvers/rds/resolver.ts
+++ packages/amplify-graphql-model-transformer/src/resolvers/rds/resolver.ts
@@ -172,10 +172,10 @@ export const createRdsLambda = (
  */
 export const createLayerVersionCustomResource = (scope: Construct, resourceNames: SQLLambdaResourceNames): AwsCustomResource => {
   const { SQLLayerManifestBucket, SQLLayerManifestBucketRegion, SQLLayerVersionManifestKeyPrefix } = ResourceConstants.RESOURCES;
-
+  const SQLLayerBetaManifestBucket = `${SQLLayerManifestBucket}-beta`;
   const key = Fn.join('', [SQLLayerVersionManifestKeyPrefix, Fn.ref('AWS::Region')]);
 
-  const manifestArn = `arn:aws:s3:::${SQLLayerManifestBucket}/${key}`;
+  const manifestArn = `arn:aws:s3:::${SQLLayerBetaManifestBucket}/${key}`;
 
   const resourceName = resourceNames.sqlLayerVersionResolverCustomResource;
   const customResource = new AwsCustomResource(scope, resourceName, {
@@ -185,7 +185,7 @@ export const createLayerVersionCustomResource = (scope: Construct, resourceNames
       action: 'getObject',
       region: SQLLayerManifestBucketRegion,
       parameters: {
-        Bucket: SQLLayerManifestBucket,
+        Bucket: SQLLayerBetaManifestBucket,
         Key: key,
       },
       // Make the physical ID change each time we do a deployment, so we always check for the latest version. This means we will never have
